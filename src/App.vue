bc9e106e20e9a5908d1ae151403fb7639a4eaf0f
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-04-29 16:11:57 +0300

    Add location and temperature elements and style

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..." />
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Nicosia, CY</div>
+          <div class="date">Monday 20, April 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">9°c</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -56,13 +67,59 @@ main {
   display: block;
   width: 100%;
   padding: 15px;
+
   color: #313131;
   font-size: 20px;
+
   appearance: none;
   border: none;
   outline: none;
   background: none;
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.5);
   border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0;
+
+  box-shadow: 3px 6px rgba(255, 255, 255, 0.25);
 }
 </style>

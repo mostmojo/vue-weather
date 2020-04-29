16651e9a05634837ecf25f60e111fbb4b254fddd
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-04-29 15:09:38 +0300

    Create search bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..." />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      api_key: "aa3a121769cf43a9c0dde00f3901c183"
+    };
+  }
 };
 </script>
 
